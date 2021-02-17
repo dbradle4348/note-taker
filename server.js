6d3f344a5618bc231197cd7d0457138560120242
King Julian <daniel.bradley09@gmail.com>
2021-02-16 18:30:50 -0700

    thing

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get('/', (req, res) => {
@@ -12,6 +12,6 @@ app.get('/notes', (req, res) => {
 
 
 
-app.listen(3001, () => {
-    console.log('API server now on port 3001!')
-})
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });

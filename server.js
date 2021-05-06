e6cc607ce57548375022a76217bdd8275fb24f04
dbradle4348 <73002887+dbradle4348@users.noreply.github.com>
2021-05-06 17:17:45 -0600

    server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));

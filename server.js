7e2348697dc3c88050bda24bf3f51ba3a62067fe
dbradle4348 <73002887+dbradle4348@users.noreply.github.com>
2021-05-06 19:50:06 -0600

    started routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
+const fs = require('fs');
 const app = express();
 
 
@@ -11,6 +12,26 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
+
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+  });
+
+  app.post('/api/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+  
+    if (!validateAnimal(req.body)) {
+      res.status(400).send('The animal is not properly formatted.');
+    } else {
+      const animal = createNewAnimal(req.body, animals);
+      res.json(animal);
+    }
+  });
 
 
 

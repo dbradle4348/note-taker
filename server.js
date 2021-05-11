0a838894e69def11203d22150d53bbc40c529e6a
dbradle4348 <73002887+dbradle4348@users.noreply.github.com>
2021-05-10 19:54:25 -0600

    feature/js

--- server.js
+++ server.js
@@ -10,41 +10,42 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
-    let results = notes;
-    if (req.query) {
-      results = filterByQuery(req.query, results);
-    }
-    res.json(results);
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+      db = JSON.parse(data);
+      res.send(dbData);
+    })
   });
 
 app.post('/api/notes', (req, res) => {
-    let newNote = req.body;
+    const userNotes = req.body;
     // set unique id
     newNote.id = uniqid();
   
-    readFile("./db/db.json", "utf8")
-    .then((result, err) => {
-    return Promise.resolve(JSON.parse(result));
-  })
-  
-    .then(data => {
-          //write new file
-          writeFile("./db/db.json", JSON.stringify(data));
-          res.json(newNote);
-    })      
-    .catch(err =>{
-        if(err) throw err;
-    
-      })
-  });
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) throw err;
+      dbData = JSON.parse(data);
+      let number = 1;
+      dbData.forEach((note, index) => {
+        note.id = number;
+        number++;
+        return dbData;
+      });
+      stringData = JSON.stringify(dbData);
+      fs.writeFile('./db/db.json', stringData, (err, data) => {
+        if (err) throw err;
+      });
+    });
+});
+
 
 
 

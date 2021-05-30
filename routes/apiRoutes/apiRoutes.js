c0c31fc2424c5078f0a7ca3fcc25c9d5ffebefd7
dbradle4348 <73002887+dbradle4348@users.noreply.github.com>
2021-05-29 21:42:10 -0600

    adjusting post route

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,38 +1,27 @@
-const router = require('express').Router();
-const fs = require('fs');
-
+const router = require("express").Router();
+const fs = require("fs");
 
 //get notes
-router.get('/notes', (req, res) => {
-    fs.readFile('./db/db.json', (err, data) => {
-      if (err) throw err;
-      dbData = JSON.parse(data);
-      res.send(dbData);
-    })
+router.get("/notes", (req, res) => {
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) throw err;
+    dbData = JSON.parse(data);
+    res.send(dbData);
   });
-
-//post routes
-router.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-  
-    fs.readFile("./db/db.json", (err, data) => {
-      if (err) throw err;
-      dbData = JSON.parse(data);
-      dbData.push(newNote);
-      let number = 1;
-      dbData.forEach((note, index) => {
-        note.id = number;
-        number++;
-        return dbData;
-      });
-      console.log(dbData);
-      noteData = JSON.stringify(dbData);
-      fs.writeFile('./db/db.json', noteData, (err, data) => {
-        if (err) throw err;
-      });
-    });
-    res.send("Note posted!")
 });
 
+//post routes
+router.post("/api/notes", (req, res) => {
+  const userNote = req.body;
+
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) throw err;
+    const notesArray = JSON.parse(data);
+    userNote.id = generateUniqueId({ length: 8 });
+    notesArray.push(userNote);
+    console.log("Note Posted!");
+    res.send(notesArray);
+  });
+});
 
 module.exports = router;
